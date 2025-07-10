2e1576b99c20e9bec0d6f4d7b34c1ed183d6f4e0
Teerapat Jaisai <teerapat.jai@ku.th>
2025-07-10 15:09:25 +0700

    error

---
+++ assignments/week01/lab01.py
@@ -0,0 +1,23 @@
+# Exercise 1: Personal Profile Creator
+print("=== Personal Profile Creator ===")
+# Create a program that asks for:
+# - Full name
+# - Age
+# - Email
+# - Phone number
+# - Favorite hobby
+# Then display it as a profile
+
+# Write your solution here:
+full_name = input("what is your name? : ")
+age = input("how old are you? : ")
+Email = input("what is your email address: ")
+Phone_number = input("what is your phone number? :")
+hobby = input("what is your favorite hobby? : ")
+
+print("== Your profile ===")
+print("Full name: ",full_name)
+print("age: ",age)
+print("email:,email")
+print("Phone number: ",Phone_number)
+print("Favorite hobby: ",hobby)
